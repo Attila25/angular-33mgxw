1a3a6efc8fd52370d762e1e3b9b6f8d0f77b2ce5
Attila25 <57138770+Attila25@users.noreply.github.com>
2023-05-17 15:24:23 +0200

    2 files modified

--- src/app/teachers/teachers-list/teachers-list.component.ts
+++ src/app/teachers/teachers-list/teachers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable, Subject } from 'rxjs';
 import { TeachersService } from '../teachers.service';
@@ -8,8 +8,10 @@ import {
   teacherRequestedAction,
 } from '../store/teachers.actions';
 import { TeacherModel } from '../store/teachers.model';
-
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-teachers-list',
@@ -38,9 +40,15 @@ export class TeachersListComponent implements OnInit {
     select(selectTeachers)
   );
 
+  dataSource = new MatTableDataSource<TeacherModel>();
+
   teachers_s$: Observable<TeacherModel[]>;
 
-  constructor(private teachersService: TeachersService, private store: Store) {}
+  constructor(
+    private _liveAnnouncer: LiveAnnouncer,
+    private teachersService: TeachersService,
+    private store: Store
+  ) {}
 
   ngOnInit() {
     this.teachers_s$ = this.searchTerms.pipe(
@@ -50,9 +58,32 @@ export class TeachersListComponent implements OnInit {
 
       switchMap((term: string) => this.teachersService.searchTeachers(term))
     );
+
+    this.teachers$.subscribe(
+      (data) => (this.dataSource = new MatTableDataSource(data))
+    );
+
     this.store.dispatch(teachersRequestedAction());
   }
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
   searchT() {
     this.teachers$ = this.teachers_s$;
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
