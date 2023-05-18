7fd5f982d841f3419164fccc00361446607b2eea
Attila25 <57138770+Attila25@users.noreply.github.com>
2023-05-18 16:50:46 +0200

    1 file modified

--- src/app/data/semesters.data.ts
+++ src/app/data/semesters.data.ts
@@ -10,21 +10,21 @@ export class SemesterTable {
   public static semesters: Semester[] = [
     {
       id: 1,
-      name: '2020/2021/1',
+      name: '2020/21/1',
       start_date: new Date(2020, 9, 15),
       end_date: new Date(2021, 1, 31),
       deleted: false,
     },
     {
       id: 2,
-      name: '2020/2021/2',
+      name: '2020/21/2',
       start_date: new Date(2021, 2, 15),
       end_date: new Date(2021, 6, 31),
       deleted: false,
     },
     {
       id: 3,
-      name: '2021/2022/1',
+      name: '2021/22/1',
       start_date: new Date(2021, 9, 15),
       end_date: new Date(2022, 1, 31),
       deleted: false,
