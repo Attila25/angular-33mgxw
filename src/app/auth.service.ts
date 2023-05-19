60cd8901d4230b77186e69628f97ba1fdd9d68e3
Attila25 <57138770+Attila25@users.noreply.github.com>
2023-05-19 12:05:34 +0200

    3 files modified

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 
 @Injectable()
 export class AuthService {
-
-  constructor() { }
+  constructor() {}
 
   getToken(): string {
     return 'secure-token-123';
@@ -13,4 +13,14 @@ export class AuthService {
     return true;
   }
 
+  getDecodedAccessToken(): void {
+    const token =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2ODQ0ODk1ODEsImV4cCI6MTcxNjAyNTU4MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.RC2jdG9-vwsLktPjtzXNPyZPavI35H3Altns4ZHB3D8';
+    try {
+      const tokeninfo = jwt_decode(token);
+      console.log(tokeninfo);
+    } catch (Error) {
+      const tokeninfo = null;
+    }
+  }
 }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild, CanLoad, Route } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateChild,
+  CanLoad,
+  Route,
+} from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-      return this.authService.isAuthenticated();
+    state: RouterStateSnapshot
+  ): boolean {
+    this.authService.getDecodedAccessToken();
+    return this.authService.isAuthenticated();
   }
 
   canActivateChild(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
+    state: RouterStateSnapshot
+  ): boolean {
     return this.canActivate(route, state);
   }
 
